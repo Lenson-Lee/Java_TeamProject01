86de4f97f91385a0049005155d2e1e073fbfd178
Lenson-Lee <84387116+Lenson-Lee@users.noreply.github.com>
2021-08-11 12:35:17 +0900

    Update Project.java

--- Project.java
+++ Project.java
@@ -11,9 +11,10 @@ public class Project {
     //++++++++++++++ 전역 변수 +++++++++++++++
     //=======================================
 
-    //메뉴선택에서 선택지를 제외한 단어 클릭시 다시 입력하게 하는 배열
+    //사장님 메뉴판 수정에서 사용, 메뉴선택시 이외의 선택지는 다시 입력
     static String[] badInput = {"짜장면", "짬뽕", "볶음밥", "탕수육", "칠리새우"};
-    static String a = "칠리새우";
+    // 사장님 메뉴판 수정에서 사용할 배열
+    static int[] outterCost = {5000, 6000, 7000, 12000, 30000};
 
     //전역변수로 뺄 때는 static 사용.
     static Scanner sc = new Scanner(System.in);
@@ -78,14 +79,18 @@ public class Project {
         String[] selectFood = new String[0];
 
         System.out.println("***** 메뉴를 선택 하세요 *****");
-        System.out.println("짜장면 - 5000원");
-        System.out.println("짬뽕 - 6000원");
-        System.out.println("볶음밥 - 7000원");
-        System.out.println("탕수육 - 12000원");
-        System.out.println("칠리새우 - 30000원");
+        for (int i = 0; i < badInput.length; i++) {
+            System.out.println(badInput[i] + " - " + outterCost[i] + "원");
+//            System.out.println("짜장면 - 5000원");
+//            System.out.println("짬뽕 - 6000원");
+//            System.out.println("볶음밥 - 7000원");
+//            System.out.println("탕수육 - 12000원");
+//            System.out.println("칠리새우 - 30000원");
+        }
         System.out.println("메뉴 선택이 완료 되었으면 \"선택완료\"라고 입력하세요!!");
-
+        //총 액수
         int totalCost = 0;
+
         // 주문작업
         while (true) {
             System.out.print(">> ");
@@ -159,18 +164,22 @@ public class Project {
             String lastFoodSelect = sc.next();
             sc.nextLine();
 
+
             if (lastFoodSelect.equals("예")) {
-                System.out.println("\n\n주문이 접수되었습니다!!");
                 System.out.println(" ");
                 System.out.println("주문하신 음식: " + Arrays.toString(selectFood));
-                System.out.println("\n총 금액: " + totalCost + "원");
+                System.out.println("총 금액: " + totalCost + "원");
                 System.out.println(" ");
+                requestList();
                 menuModifyDelete();
+                System.out.println("");
+                System.out.println("#  주문이 접수되었습니다!!");
 
                 total = totalCost;
                 return true;
             } else if (lastFoodSelect.equals("아니요")) {
                 System.out.println("주문을 처음부터 다시 접수하세요!!");
+                System.out.println("");
                 // selectFood 배열을 빈배열로 만드는 작업 (초기화)
                 return false;
             }
@@ -180,7 +189,7 @@ public class Project {
     // 주문한 메뉴 수정 및 삭제 함수
     //////////////////   수정해야함 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     static void menuModifyDelete() {
-        System.out.println("혹시 마음이 바뀌어서 취소하고 싶은 음식이 있으시면 \"취소\"를 입력하시고, 그대로 주문하고 싶으면 \"주문\"을 입력하세요.");
+        System.out.println("취소하고 싶은 음식이 있으시면 \"취소\"를 입력하시고, 그대로 주문하고 싶으면 \"주문\"을 입력하세요.");
         System.out.print(">> ");
         String deleteFood = sc.next();
         sc.nextLine();
@@ -211,14 +220,14 @@ public class Project {
         menuBox = deleteTemp;
         deleteTemp = null;
 
-        System.out.println("취소 후 주문하신 음식: " + Arrays.toString(menuBox));
+        System.out.println("주문하신 음식: " + Arrays.toString(menuBox));
 
     } // end menuModifyDelete
 
 
     // 요청사항 함수
     static void requestList() {
-        System.out.println("\n\n음식 주문 요청사항을 입력하세요!!");
+        System.out.println("\n음식 주문 요청사항을 입력하세요!!");
         while (true) {
             System.out.print(">> ");
 
@@ -290,7 +299,8 @@ public class Project {
         Date time = new Date();
         String time2 = format2.format(time);
 
-        System.out.println("\n\n*****최근 주문목록 *****");
+        System.out.println("");
+        System.out.println("*****최근 주문목록 *****");
         if (menuBox.length != 0) {
             for (int i = 0; i < menuBox.length; i++) {
                 System.out.println(time2 + " 주문한 음식: " + menuBox[i]);
@@ -299,17 +309,15 @@ public class Project {
                 System.out.println("[요청사항: " + requestBox[i] + "]");
             }
             System.out.println(" ");
-            System.out.println(" ");
-            guestForm();
         } else {
             System.out.println("최근에 주문한 것이 없습니다. 많이 이용해주세요!!");
             System.out.println(" ");
-            System.out.println(" ");
-            guestForm();
         }
     }
 
-    //게스트 폼
+    //=====================================
+    //              게스트 폼
+    //=====================================
     static void guestForm() {
         System.out.println("=================================");
         System.out.println("1. 메뉴 선택");
@@ -320,19 +328,22 @@ public class Project {
         System.out.println("원하는 메뉴 번호를 입력해 주세요!");
 
         int menu = sc.nextInt();
+        System.out.println("");
 
         switch (menu) {
             case 1:
                 System.out.println("1. 메뉴 선택");
                 menuSelect();
-                requestList();
+                guestForm();
                 break;
             case 2:
                 System.out.println("2. 내 주문 기록");
                 orderList();
+                guestForm();
                 break;
             case 3:
                 System.out.println("3. 로그아웃");
+                login();
                 break;
             case 4:
                 System.out.println("4. 프로그램 종료");
@@ -348,7 +359,8 @@ public class Project {
     //+++++++++++++사장님 전용 폼++++++++++++++
     //=======================================
 
-    //주문서 확인함수
+    //사장님 폼 - 신규 주문서 확인함수
+    //주문 취소 시 메인 폼으로 돌아간다.
     static void OrderView() {
         System.out.println("주문이 들어왔습니다!" + Arrays.toString(menuBox));
         System.out.println("총 금액 : " + total + "원");
@@ -358,25 +370,33 @@ public class Project {
         System.out.println("1. 예 2. 아니요 취소하겠습니다.");
         int choose = sc.nextInt();
         sc.nextLine();
+        System.out.println("");
         switch (choose){
             case 1:
                 System.out.println("주문이 수락되었습니다!");
+                System.out.println("메뉴로 돌아갑니다!");
+                System.out.println("");
                 break;
             case 2:
-                //주문 취소 => 주문 배열 비우기
+                //주문 취소 => 주문 배열 비우고 메인 폼으로 돌아가기
                 System.out.println("주문이 취소되었습니다!");
-                menuBox = null;
+                System.out.println("메뉴로 돌아갑니다!");
+                System.out.println("");
+
+                menuBox = new String[0];
+                total = 0;
                 break;
         }
     }
+
+    //사장님 폼 - 3. 최근 주문기록 조회하기
     static void historyMaster() {
         //기본값 음식 기록들
         String[] history = {"[08.02]숟가락님의 주문입니다", "[08.03]밥그릇님의 주문입니다.", "[08.03]뚝배기님의 주문입니다."};
         int[] price = {13500, 28000, 10000};
         int total = 0;
-//
+
 //        System.out.println("3. 최근 주문기록 조회하기");
-        System.out.println("\n=================================");
         System.out.println("이번달 주문내역은 총" + history.length + "개 입니다.");
 
         for (int i = 0; i < price.length; i++) {
@@ -384,11 +404,11 @@ public class Project {
         }
 
         System.out.printf("이번달 총 수익은 %d원 입니다.\n", total);
-        System.out.println("\n");
+        System.out.println("");
 
     }
 
-
+    //사장님 폼 메인 화면
     static void masterForm() {
         System.out.println("=================================");
         System.out.println("1. 메뉴판 수정하기");
@@ -398,25 +418,28 @@ public class Project {
         System.out.println("5. 프로그램 종료");
         System.out.println("=================================");
         System.out.println("원하는 메뉴 번호를 입력해 주세요!");
-        System.out.println("");
-
         int menu = sc.nextInt();
+        System.out.println("");
 
         switch (menu) {
             case 1:
                 System.out.println("1. 메뉴판 수정이 메뉴판 배열이 없어서 불가능");
                 System.out.println("");
+                masterForm();
                 break;
             case 2:
                 System.out.println("2. 주문서 조회하기"); //+ 주문취소 여부
                 OrderView();
+                masterForm();
                 break;
             case 3:
                 System.out.println("3. 최근 주문기록 조회하기");
                 historyMaster();
+                masterForm();
                 break;
             case 4:
                 System.out.println("3. 로그아웃");
+                login();
                 break;
             case 5:
                 System.out.println("4. 프로그램 종료");
@@ -424,42 +447,54 @@ public class Project {
         }
     }
 
-
-    // ===================================================
-    // +++++++++++++++++메인 실행 함수+++++++++++++++++++++
-    // ===================================================
-
-    public static void main(String[] args) {//메인함수
-
+    //==================================
+    //++++++++++++로그인 함수+++++++++++++
+    //손님, 사장님 로그인에 따라 폼이 구분된다.
+    static void login() {
         System.out.println("=================================");
         System.out.println("\n++++++++++주문서 프로그램++++++++++\n");
         System.out.println("=================================");
-        System.out.println("로그인을 해주세요!\n");
+        System.out.println("로그인이 필요합니다!");
+        System.out.println("id를 입력 해주세요!");
         System.out.print(">> ");
-        System.out.println("id를 입력해 주세요!");
         String id = sc.next();
+        System.out.println("");
 
         switch (id) {
             case "master"://사장님 계정, 비밀번호 일치시 함수 실행
                 System.out.println("사장님 계정 입니다.");
                 System.out.println("사장님 비밀번호를 입력해 주세요!");
                 String pw = sc.next();
+                System.out.println("");
                 String realPw = "abc";
 
-
-                    if (pw.equals(realPw)) {
-                        System.out.println("사장님 어서오세요!\n");
-                        masterForm();
-                    } else {
-                        System.out.println("비밀번호가 틀렸습니다. 프로그램을 종료합니다.");
-                        break;
-                    }
-
+                if (pw.equals(realPw)) {
+                    System.out.println("사장님 어서오세요!");
+                    System.out.println("");
+                    //로그인시 사장님 폼으로 이동.
+                    masterForm();
+                    break;
+                } else {
+                    System.out.println("비밀번호가 틀렸습니다. 다시 로그인 해 주세요.");
+                    System.out.println("");
+                    //비밀번호 실패 시 재 로그인 필요.
+                    login();
+                    break;
+                }
             default: //손님 계정. 아이디 일치 후 비밀번호 확인 => 게스트함수 실행
                 guestLogin(id);
                 break;
         }
+    }
 
+    // ===================================================
+    // +++++++++++++++++메인 실행 함수+++++++++++++++++++++
+    // ===================================================
+
+    public static void main(String[] args) {//메인함수
+        //로그인 창으로 돌아갈 수 있도록 로그인 함수 login()으로 시작.
+        //로그인 확인 후 각자 손님/사장님 폼으로 이동 후 작동.
+        login();
 
     }//class end
 }
